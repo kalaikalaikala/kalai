48f02b42eea9cafd244f998ebd5d83a6b582b38f
kalaiyarasi <137065759+kalaikalaikala@users.noreply.github.com>
2023-06-24 20:51:50 +0530

    Add files via upload

---
+++ my first programe.py
@@ -0,0 +1,16 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/jabir/OneDrive/Documents/Python Scripts/கலை/sample/name.py
+hello, world!
+what is your name?kalai
+hello kalai
+>>> height=158
+>>> print(height)
+158
+>>> age=15
+>>> print(age)
+15
+>>> weight=52
+>>> print(weight)
+52
